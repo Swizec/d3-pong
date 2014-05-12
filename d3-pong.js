b8191d6ecb7cda62d2ab887e4a49dc068ac077cb
Swizec Teller <swizec@swizec.com>
2014-05-11 23:30:50 -0700

    scene reacts to screen size changes

--- d3-pong.js
+++ d3-pong.js
@@ -1,3 +1,4 @@
+"use strict";
 
 (function () {
 
@@ -17,43 +18,56 @@
         Paddle = function () {
             var paddle = svg.append('rect')
                     .attr({width: 5});
-            return function (x, y) {
-                return paddle.attr({
+
+            return function f(x, y) {
+                paddle.attr({
                     x: x,
                     y: y,
-                    height: Screen().height*0.05
+                    height: Screen().height*0.1
                 });
+                return f;
             };
         },
-        Score = function (x, y) {
-            var score = svg.append('text')
-                    .attr({
-                        x: x,
-                        y: y
-                    });
-            return function (value) {
-                return score.text(value);
+        Score = function (x) {
+            var score = svg.append('text');
+
+            return function f(value) {
+                score.text(value)
+                    .attr({x: Screen().width*x,
+                           y: margin.top*3});
+                return f;
             };
         },
         Middle = function () {
             var line = svg.append('line');
-            return function () {
+
+            return function f() {
                 var screen = Screen();
-                return line.attr({
+                line.attr({
                     x1: screen.width/2,
                     y1: margin.top,
                     x2: screen.width/2,
                     y2: screen.height-margin.bottom
                 });
+                return f;
             };
         };
     
 
-    var left = {score: Score(Screen().width*0.25, margin.top*3)(0),
+    var left = {score: Score(0.25)(0),
                 paddle: Paddle()(margin.left, Screen().height/2)},
-        right = {score: Score(Screen().width*0.75, margin.top*3)(0),
+        right = {score: Score(0.75)(0),
                 paddle: Paddle()(Screen().width-margin.right, Screen().height/2)},
         middle = Middle()();
     
-    
+    d3.select(window).on('resize', function () {
+        var screen = Screen();
+
+        left.score(0);
+        left.paddle(margin.left, screen.height/2);
+        right.score(0);
+        right.paddle(screen.width-margin.right, screen.height/2);
+
+        middle();
+    });
 })();
